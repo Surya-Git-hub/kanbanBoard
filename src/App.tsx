01775e2f8af951477cf127d5f38a1bed251d3d06
surya kant <suryakant0013@yahoo.in>
2023-08-08 00:06:31 +0530

    fixed App syntax errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
-import { useState } from "react";
 import "./App.css";
 import Column from "./components/Column";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className= "App">
+    <div className="App">
       <Column state="PLANNED"></Column>
       <Column state="ONGOING"></Column>
       <Column state="DONE"></Column>
-    </div >
+    </div>
   );
 }
 
